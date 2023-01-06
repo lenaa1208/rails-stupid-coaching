e7750c7b1d79fb2680a64c29c29d8fd30cc4b947
Lena Ansari <lena.ansari@gmail.com>
2023-01-06 01:11:18 +0100

    done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,18 @@
-class PagesController < ApplicationController
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    if @question.blank?
+      @answer = "I can't hear you!"
+    elsif @question =~ /I am going to work/i
+      @answer = 'Great!'
+    elsif @question.end_with?("?")
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work"
+    end
+  end
 end
